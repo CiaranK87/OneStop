44088f2b9aa719d15f8c613722e01d4304749fa7
Ciaran <ciarank500@gmail.com>
2023-07-12 10:17:36 +0100

    Basic UI with material ui

--- API/Data/DbInitializer.cs
+++ API/Data/DbInitializer.cs
@@ -16,7 +16,7 @@ namespace API.Data
                     Description =
                         "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
                     Price = 20000,
-                    PictureUrl = "/images/products/sb-ang1.png",
+                    PictureUrl = "/images/products/theEgg.jpg",
                     Brand = "Angular",
                     Type = "Boards",
                     QuantityInStock = 100
